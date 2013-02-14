95f817dc3fffabd0b645b37591479df871632079
Konstantin Tarkus <hello@tarkus.me>
2013-02-14 15:53:07 +0400

    Import historical EOD quotes; Update TOP 50 stock list

--- Plugin/DataSource.cs
+++ Plugin/DataSource.cs
@@ -7,11 +7,51 @@
 namespace AmiBroker.Plugin
 {
     using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using System.Web.Script.Serialization;
 
-    using AmiBroker.Plugin.Models;
+    using Models;
 
     public class DataSource
     {
+        public class Market
+        {
+            public string ID { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class Symbol
+        {
+            public string ID { get; set; }
+
+            public string Ticker { get; set; }
+
+            public string FullName { get; set; }
+
+            public string MarketID { get; set; }
+
+            public int MarketIndex { get; set; }
+
+        }
+
+        private readonly string baseUrl = "http://www.finam.ru";
+
+        private string exportUrl = null;
+
+        private string exportEndpoint = null;
+
+        private Dictionary<string, Tuple<DateTime, Quotation[]>> cache = new Dictionary<string, Tuple<DateTime, Quotation[]>>();
+
+        private Market[] markets;
+
+        private Symbol[] symbols;
+
         public DataSource(string databasePath, IntPtr mainWnd)
         {
             this.DatabasePath = databasePath;
@@ -31,11 +71,186 @@ namespace AmiBroker.Plugin
         /// </summary>
         public dynamic Broker { get; private set; }
 
+        public async Task<string> GetExportQuotesUrl()
+        {
+            if (string.IsNullOrWhiteSpace(this.exportUrl))
+            {
+                using (var http = new HttpClient())
+                {
+                    var html = await http.GetStringAsync(baseUrl);
+                    var match = Regex.Match(html, "href=\"(.*?)\">Экспорт данных<");
+
+                    if (!match.Success)
+                    {
+                        throw new InvalidOperationException("Failed to obtain the export quotes URL from the www.finam.ru website.");
+                    }
+
+                    this.exportUrl = match.Groups[1].Value;
+                    Debug.WriteLine("Export quotes URL found: " + this.exportUrl);
+                }
+            }
+
+            return this.exportUrl;
+        }
+
+        public async Task<string> GetExportQuotesEndpoint()
+        {
+            if (string.IsNullOrWhiteSpace(this.exportEndpoint))
+            {
+                using (var http = new HttpClient())
+                {
+                    http.BaseAddress = new Uri(baseUrl);
+                    var url = await this.GetExportQuotesUrl();
+                    var html = await http.GetStringAsync(url);
+                    var match = Regex.Match(html, "id=\"chartform\" action=\"http://(.*?)/");
+
+                    if (!match.Success)
+                    {
+                        throw new InvalidOperationException("Failed to obtain the quotes endpoint URL from the http://www.finam.ru" + this.exportUrl);
+                    }
+
+                    this.exportEndpoint = "http://" + match.Groups[1].Value + "/";
+                    Debug.WriteLine("Quotes endpoint found: " + this.exportEndpoint);
+                }
+            }
+
+            return this.exportEndpoint;
+        }
+
+        public async Task<Market[]> GetMarkets()
+        {
+            if (this.markets == null)
+            {
+                using (var http = new HttpClient())
+                {
+                    http.BaseAddress = new Uri(baseUrl);
+                    var url = await this.GetExportQuotesUrl();
+                    var html = await http.GetStringAsync(url);
+                    var match = Regex.Match(html, @"Finam\.IssuerProfile\.Main\.setMarkets\(\[(.*?)\]\);", RegexOptions.Singleline);
+                    this.markets = match.Groups[1].Value.TrimEnd('}').Split(new string[] { "}, " }, StringSplitOptions.None).Select(x =>
+                    {
+                        var split = x.Split(',');
+                        return new Market { ID = split[0].Substring(9), Name = split[1].Substring(9, split[1].Length - 10) };
+                    }).ToArray();
+                }
+            }
+
+            return this.markets;
+        }
+
+        public async Task<Symbol[]> GetSymbols()
+        {
+            if (this.symbols == null)
+            {
+                var markets = await this.GetMarkets();
+
+                using (var http = new HttpClient())
+                {
+                    string html;
+                    var js = new JavaScriptSerializer();
+                    http.BaseAddress = new Uri(baseUrl);
+
+                    using (var stream = await http.GetStreamAsync("/cache/icharts/icharts.js"))
+                    using (var reader = new System.IO.StreamReader(stream, System.Text.Encoding.GetEncoding("windows-1251")))
+                    {
+                        html = reader.ReadToEnd();
+                    }
+
+                    var matches = Regex.Matches(html, @"\[(.*?)\]");
+                    var ids = js.Deserialize<string[]>("[" + matches[0].Groups[1].Value + "]");
+                    var names = js.Deserialize<string[]>("[" + matches[1].Groups[1].Value + "]");
+                    var codes = js.Deserialize<string[]>("[" + matches[2].Groups[1].Value + "]");
+                    var marketids = js.Deserialize<string[]>("[" + matches[3].Groups[1].Value + "]");
+
+                    this.symbols = ids.Select((id, i) => new Symbol { ID = id, MarketID = marketids[i], Ticker = codes[i], FullName = names[i], MarketIndex = markets.ToList().FindIndex(x => x.ID == marketids[i]) }).ToArray();
+                }
+            }
+
+            return this.symbols;
+        }
+
         public Quotation[] GetQuotes(string ticker, Periodicity periodicity, int limit, Quotation[] lastQuotes = null)
         {
-            // TODO: Return the list of quotes for the specified ticker.
+            // Check if there is already a portion of quotes waiting to be returned to the caller
+            if (this.cache.ContainsKey(ticker))
+            {
+                var quotes = this.cache[ticker];
+                
+                // Check if quotes were fetched less than 5 minutes ago
+                if (quotes.Item1 > DateTime.Now.AddMinutes(-5))
+                {
+                    var result = quotes.Item2;
+                    this.cache.Remove(ticker);
+                    return result;
+                }
+            }
 
-            return new Quotation[] { };
+            // If not, fetch new quotes asynchroniously
+            Task.Run(async () =>
+            {
+                var symbol = (await this.GetSymbols()).FirstOrDefault(x => x.Ticker == ticker);
+
+                if (symbol == null)
+                {
+                    Debug.WriteLine("Symbol " + ticker + " was not found.");
+                    return;
+                }
+
+                if (periodicity == Periodicity.EndOfDay)
+                {
+                    var quotesFileName = System.IO.Path.Combine(this.DatabasePath, ticker + ".csv");
+                    var formatFileName = System.IO.Path.Combine(this.DatabasePath, "quotes.format");
+
+                    var tz = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
+                    var lastDate = lastQuotes == null ? null : new AmiDate(lastQuotes[0].DateTime);
+                    var startDate = lastQuotes == null ? DateTime.Today.AddDays(-((double)limit / 5 * 7 + 10)) : new DateTime(lastDate.Year, lastDate.Month, lastDate.Day);
+                    var endDate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow.Date, tz);
+
+                    Debug.WriteLine("Downloading " + ticker + " quotes starting from " + startDate.ToShortDateString() + " to " + endDate.ToShortDateString());
+
+                    string csv = null;
+
+                    using (var http = new HttpClient())
+                    {
+                        var url = await this.GetExportQuotesEndpoint();
+                        Debug.WriteLine("Setting base url " + url);
+                        http.BaseAddress = new Uri(url);
+                        url = string.Format("/{0}.csv?market={1}&em={2}&code={0}&df={3:dd}&mf={3:mm}&yf={3:yyyy}&dt={4:dd}&mt={4:mm}&yt={4:yyyy}&p=8&f={0}&e=.csv&cn={0}&dtf=1&tmf=1&MSOR=0&mstime=on&mstimever=1&sep=1&sep2=2&datf=1", ticker, symbol.MarketID, symbol.ID, startDate, endDate);
+                        Debug.WriteLine(url);
+                        csv = await http.GetStringAsync(url);
+                        Debug.WriteLine(csv.Substring(0, 200));
+                    }
+
+                    Debug.WriteLine("Saving quotes for " + ticker + " to " + quotesFileName);
+
+                    using (var fs = System.IO.File.Open(quotesFileName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write, System.IO.FileShare.None))
+                    using (var sw = new System.IO.StreamWriter(fs))
+                    {
+                        fs.Position = 0;
+                        sw.Write(csv);
+                        sw.Flush();
+                        fs.SetLength(fs.Position);
+                    }
+
+                    Debug.WriteLine("Creating " + formatFileName);
+
+                    using (var fs = System.IO.File.Open(formatFileName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write, System.IO.FileShare.None))
+                    using (var sw = new System.IO.StreamWriter(fs))
+                    {
+                        fs.Position = 0;
+                        sw.WriteLine("$FORMAT Ticker,Skip,Date_YMD,Skip,Open,High,Low,Close,Volume\n$SKIPLINES 0\n$SEPARATOR ,\n$DEBUG 1\n$AUTOADD 1\n$CONT 1\n$GROUP 254\n$BREAKONERR 1");
+                        sw.Flush();
+                        fs.SetLength(fs.Position);
+                    }
+
+                    Debug.WriteLine("Importing quotes for " + ticker + " from " + quotesFileName);
+
+                    this.Broker.Import(0, quotesFileName, formatFileName);
+                    this.Broker.RefreshAll();
+                }
+            });
+
+            return new Quotation[0];
         }
     }
 }
--- Plugin/PluginControl.xaml.cs
+++ Plugin/PluginControl.xaml.cs
@@ -9,12 +9,14 @@ namespace AmiBroker.Plugin
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Net.Http;
     using System.Text;
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
+    using System.Web;
     using System.Web.Script.Serialization;
     using System.Windows;
     using System.Windows.Controls;
@@ -24,11 +26,12 @@ namespace AmiBroker.Plugin
     /// </summary>
     public partial class PluginControl : UserControl
     {
-        internal struct market
+        private class EodData
         {
-            public int value;
-            public string title;
+            public string[][] rows;
         }
+
+        private readonly DataSource dataSource;
 
         public PluginControl(DataSource dataSource)
         {
@@ -36,83 +39,112 @@ namespace AmiBroker.Plugin
             this.InitializeComponent();
         }
 
-        private readonly DataSource dataSource;
-
         private async void UpdateSymbolList_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Update the symbol list");
+
+            var marekts = await this.dataSource.GetMarkets();
+
+            Debug.WriteLine(marekts.Length + " markets found");
+
+            var symbols = await this.dataSource.GetSymbols();
+
+            Debug.WriteLine(symbols.Length + " symbols found");
+
+            var fileName = Path.Combine(this.dataSource.DatabasePath, "symbols.csv");
+            var fileName2 = Path.Combine(this.dataSource.DatabasePath, "symbols.format");
+
+            using (var fs = File.Open(fileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            using (var sw = new StreamWriter(fs, Encoding.GetEncoding("windows-1251")))
+            {
+                fs.Position = 0;
+                sw.WriteLine("Ticker,FullName,MarketID,WebID");
+
+                foreach (var symbol in symbols)
+                {
+                    sw.WriteLine(symbol.Ticker + "," + symbol.FullName + "," + symbol.MarketIndex.ToString("G") + "," + symbol.ID);
+                }
+
+                sw.Flush();
+                fs.SetLength(fs.Position - 2);
+            }
+
+            using (var fs = File.Open(fileName2, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            using (var sw = new StreamWriter(fs))
+            {
+                fs.Position = 0;
+                sw.WriteLine("$FORMAT Ticker,FullName,MarketID,Skip\n$SKIPLINES 1\n$SEPARATOR ,\n$CONT 1\n$GROUP 255\n$AUTOADD 1\n$DEBUG 1\n$NOQUOTES 1"); /* WebID works in AmiBroker 5.60.1 and up*/
+                sw.Flush();
+                fs.SetLength(fs.Position);
+            }
+
+            Debug.WriteLine("Importing symbols from " + fileName);
+
+            int result = this.dataSource.Broker.Import(0, fileName, fileName2);
+
+            Debug.WriteLine("Import() returned " + result);
+
+            if (result != 0)
+            {
+                MessageBox.Show("Failed to import symbols from " + fileName, "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            for (var i = 0; i < marekts.Length; i++)
+            {
+                this.dataSource.Broker.Markets.Item(i).Name = marekts[i].Name;
+                Debug.Write(marekts[i].Name + ",");
+            }
+
+            this.dataSource.Broker.RefreshAll();
+    }
+
+        private async void UpdateTop50_Click(object sender, RoutedEventArgs e)
         {
             using (var http = new HttpClient())
             {
-                http.BaseAddress = new Uri("http://www.finam.ru");
-                var html = await http.GetStringAsync("/analysis/profile041CA00007/default.asp");
-                var marketsJsonText = Regex.Match(html, @"Finam\.IssuerProfile\.Main\.setMarkets\(\[(.*?)\]\);", RegexOptions.Singleline).Groups[1].Value.Replace("'", "\"");
+                var html = await http.GetStringAsync("http://quote.rbc.ru/exchanges/demo/micex.0/daily");
+                var match = Regex.Match(html, @"var preload_stock_data = eval\('\(\[(.*?)\]\)'\);");
+                
+                if (!match.Success)
+                {
+                    MessageBox.Show("Failed to load the list of most active stocks", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                html = match.Groups[1].Value;
+                var regex = new Regex(@"\\[uU]([0-9A-Fa-f]{4})");
+                html = regex.Replace(html, m => ((char)int.Parse(m.Value.Substring(2), NumberStyles.HexNumber)).ToString());
+                html = regex.Replace(html, m => ((char)int.Parse(m.Value.Substring(2), NumberStyles.HexNumber)).ToString());
+                Debug.WriteLine("\n" + html + "\n");
                 var js = new JavaScriptSerializer();
-                var marketsTemp = js.Deserialize<market[]>("[" + marketsJsonText + "]");
-                var markets = new Dictionary<int, string>(marketsTemp.Length);
+                var data = js.Deserialize<EodData>(html);
+                Debug.WriteLine("Symbols found: " + data.rows.Length);
+                var items = data.rows.Select(x => new Tuple<string, string, double>(x[11], x[0], double.Parse(x[7]))).OrderByDescending(x => x.Item3).Take(50);
 
-                for (var i = 0; i < marketsTemp.Length; i++)
-                {
-                    markets[marketsTemp[i].value] = marketsTemp[i].title;
-                }
+                var stocksFileName = Path.Combine(this.dataSource.DatabasePath, "top50.csv");
+                var formatFileName = Path.Combine(this.dataSource.DatabasePath, "top50.format");
 
-                var marketIDs = markets.Select(x => x.Key).ToArray();
-
-                using (var stream = await http.GetStreamAsync("/cache/icharts/icharts.js"))
-                using (var reader = new StreamReader(stream, Encoding.GetEncoding("windows-1251")))
-                {
-                    html = reader.ReadToEnd();
-                }
-
-                var matches = Regex.Matches(html, @"\[(.*?)\]");
-                var ids = js.Deserialize<int[]>("[" + matches[0].Groups[1].Value + "]");
-                var names = js.Deserialize<string[]>("[" + matches[1].Groups[1].Value + "]");
-                var codes = js.Deserialize<string[]>("[" + matches[2].Groups[1].Value + "]");
-                var marketids = js.Deserialize<int[]>("[" + matches[3].Groups[1].Value + "]");
-
-                int marketID;
-                var sb = new StringBuilder();
-                sb.AppendLine("Ticker,FullName,MarketID");
-
-                for (var i = 0; i < ids.Length; i++)
-                {
-                    marketID = Array.IndexOf<int>(marketIDs, marketids[i]);
-                    sb.AppendLine(codes[i] + "," + names[i] + "," + marketID.ToString("G"));
-                }
-
-                var fileName = Path.Combine(this.dataSource.DatabasePath, "symbols.csv");
-                var fileName2 = Path.Combine(this.dataSource.DatabasePath, "symbols.format");
-
-                using (var fs = File.Open(fileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
-                using (var sw = new StreamWriter(fs, Encoding.GetEncoding("windows-1251")))
-                {
-                    fs.Position = 0;
-                    sw.WriteLine("Ticker,FullName,MarketID");
-
-                    for (var i = 0; i < ids.Length; i++)
-                    {
-                        marketID = Array.IndexOf<int>(marketIDs, marketids[i]);
-                        sw.WriteLine(codes[i] + "," + names[i] + "," + marketID.ToString("G"));
-                    }
-
-                    sw.Flush();
-                    fs.SetLength(fs.Position - 2);
-                }
-
-                using (var fs = File.Open(fileName2, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+                using (var fs = File.Open(stocksFileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
                 using (var sw = new StreamWriter(fs))
                 {
                     fs.Position = 0;
-                    sw.WriteLine("$FORMAT Ticker, FullName, MarketID\n$SKIPLINES 1\n$SEPARATOR ,\n$CONT 1\n$GROUP 255\n$AUTOADD 1\n$DEBUG 1\n$NOQUOTES 1");
+                    foreach (var item in items)
+                    {
+                        sw.WriteLine(item.Item1 + ",0");
+                    }
                     sw.Flush();
                     fs.SetLength(fs.Position);
                 }
 
-                this.dataSource.Broker.Import(0, fileName, fileName2);
-
-                for (var i = 0; i < marketsTemp.Length; i++)
+                using (var fs = File.Open(formatFileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+                using (var sw = new StreamWriter(fs))
                 {
-                    this.dataSource.Broker.Markets.Item(i).Name = marketsTemp[i].title;
+                    fs.Position = 0;
+                    sw.WriteLine("$FORMAT TICKER,WATCHLIST\n$SKIPLINES 0\n$SEPARATOR ,\n$CONT 1\n$GROUP 255\n$AUTOADD 0\n$DEBUG 1\n$NOQUOTES 1");
+                    sw.Flush();
+                    fs.SetLength(fs.Position);
                 }
 
+                this.dataSource.Broker.Import(0, stocksFileName, formatFileName);
                 this.dataSource.Broker.RefreshAll();
             }
         }
