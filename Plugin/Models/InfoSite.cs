14b473bbc13f0bafde7290b5b8d838dd0448132a
Konstantin Tarkus <hello@tarkus.me>
2013-03-01 13:34:40 +0400

    Add RecentInfo, InfoSite, SiteInterface...

---
+++ Plugin/Models/InfoSite.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InfoSite.cs" company="KriaSoft LLC">
+//   Copyright © 2013 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AmiBroker.Plugin.Models
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public enum InfoSiteCategory
+    {
+        Market,
+        Group,
+        Sector,
+        Industry,
+        Watchlist
+    }
+
+    [StructLayoutAttribute(LayoutKind.Sequential)]
+    public struct InfoSite
+    {
+        public int StructSize;
+
+        public GetStockQtyDelegate GetStockQty;
+
+        public SetCategoryNameDelegate SetCategoryName;
+
+        public GetCategoryNameDelegate GetCategoryName;
+
+        public SetIndustrySectorDelegate SetIndustrySector;
+
+        public GetIndustrySectorDelegate GetIndustrySector;
+
+        public AddStockNewDelegate AddStockNew;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        public delegate int GetStockQtyDelegate();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        public delegate int SetCategoryNameDelegate(int category, int item, string name);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public delegate string GetCategoryNameDelegate(int category, int item);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        public delegate int SetIndustrySectorDelegate(int industry, int sector);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        public delegate int GetIndustrySectorDelegate(int industry);
+
+        // Only available if called from AmiBroker 5.27 or higher
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        public delegate IntPtr AddStockNewDelegate([MarshalAs(UnmanagedType.LPStr)] string ticker);
+    }
+}
---
+++ Plugin/Models/OptimizeItem.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OptimizeItem.cs" company="KriaSoft LLC">
+//   Copyright © 2013 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AmiBroker.Plugin.Models
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayoutAttribute(LayoutKind.Sequential)]
+    public struct OptimizeItem
+    {
+        public string Name;
+
+        public float Default;
+
+        public float Min;
+
+        public float Max;
+
+        public float Step;
+
+        public double Current;
+
+        public float Best;
+    }
+}
